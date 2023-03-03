d2f33c9441ed37dfb30d3923f672fc7bc73055c9
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-03-03 21:11:49 +0000

    create app.config

---
+++ app.config.ts
@@ -0,0 +1,8 @@
+export default defineAppConfig({
+  waNo: "0822-2155-6633",
+  waUrl: "https://wa.me/6282221556633",
+  emailAddress: "info@indomascot.com",
+  igUrl: "https://www.instagram.com/indomascot/?hl=id",
+  youtubeUrl: "https://www.youtube.com/c/Indomascot/",
+  googleBusinessUrl: "https://g.page/indomascot",
+});
