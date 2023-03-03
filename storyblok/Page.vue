dde0c347226af741cf6eeab73a4c553745cc1313
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-03-03 22:32:10 +0000

    add storyblok components

---
+++ storyblok/Page.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+defineProps({
+  blok: {
+    type: Object,
+    required: true,
+  },
+});
+
+// mounted() {
+//   jQuery.featherlight.prototype.afterContent = function () {
+//     const caption = this.$currentTarget.find("img").attr("alt");
+//     this.$instance.find(".caption").remove();
+//     jQuery(
+//       '<div class="caption text-center w-full font-semibold text-slate-300 mt-1">'
+//     )
+//       .text(caption)
+//       .appendTo(this.$instance.find(".featherlight-content"));
+//   };
+//   jQuery.featherlight.prototype.onResize = function () {
+//     this.$content.css("width", "").css("height", "");
+//   };
+// },
+</script>
+
+<template>
+  <div v-editable="blok">
+    <component
+      :is="blok.component"
+      v-for="blok in blok.body"
+      :key="blok._uid"
+      :blok="blok"
+    />
+  </div>
+</template>
---
+++ storyblok/Photo.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+defineProps({
+  blok: {
+    type: Object,
+    required: true,
+  },
+});
+
+const alt = ref("");
+const title = ref("");
+
+// mounted() {
+//   const $t = this.$t.bind(this);
+//   const caption = this.blok.caption;
+
+//   this.alt += $t("dollMascotCostume", [caption]);
+//   this.title = $t("fullSizePhoto", [caption]);
+// },
+</script>
+
+<template>
+  <figure
+    v-editable="blok"
+    class="portfolio-item mx-auto max-w-full border border-slate-200 bg-white shadow lg:max-w-fit"
+  >
+    <a
+      :href="`${blok.photo}/m/628x628`"
+      rel="lightbox noopener"
+      target="_blank"
+      data-featherlight="image"
+      :alt="alt"
+    >
+      <NuxtImg
+        provider="storyblok"
+        loading="lazy"
+        width="342"
+        height="342"
+        sizes="xs:237px lg:342px"
+        :src="blok.photo"
+        :alt="blok.caption"
+        :title="title"
+        :placeholder="[100, 100, 10]"
+      />
+    </a>
+    <figcaption class="mb-2 hidden h-7 text-center text-sm lg:block">
+      {{ blok.caption }}
+    </figcaption>
+  </figure>
+</template>
+
+<style scoped>
+figure.portfolio-item > a img {
+  border-radius: 4px;
+}
+</style>
