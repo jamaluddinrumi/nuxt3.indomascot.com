f2a77a96f93e9b9aefe9fa25d6d3216d8b2ecaef
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-03-03 22:31:46 +0000

    setting scrollBehavior of vue-router

---
+++ router.options.ts
@@ -0,0 +1,24 @@
+import type { RouterConfig } from "@nuxt/schema";
+
+// https://router.vuejs.org/api/#routeroptions
+export default <RouterConfig>{
+  trailingSlash: true,
+  scrollBehavior(to, _from, savedPosition) {
+    return new Promise((resolve, _reject) => {
+      setTimeout(() => {
+        if (savedPosition) {
+          resolve(savedPosition);
+        } else {
+          if (to.hash) {
+            resolve({
+              el: to.hash,
+              top: 80,
+            });
+          } else {
+            resolve({ top: 0 });
+          }
+        }
+      }, 100);
+    });
+  },
+};
